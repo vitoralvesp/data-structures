8f6e34822f826ae52fb3703a4e0cde57f4688419
vitoralvesp <pereira.alves.vitor@outlook.com>
2025-01-24 20:53:31 -0300

    :fire: started implementing queue data structure :fire:

---
+++ Queue/src/staticqueue/StaticQueue.java
@@ -0,0 +1,88 @@
+package staticqueue;
+
+public class StaticQueue<T> {
+
+	private T[] queue;
+	private int size;
+	private int first;
+	private int last;
+	private int count;
+	
+	/* CONSTRUCTORS */
+	
+	StaticQueue() {}
+	
+	StaticQueue(int size) {
+		this.first = 0;
+		this.last = 0;
+		this.count = 0;
+		this.size = size;
+		this.queue = (T[]) new Object[size];
+	}
+	
+	
+	/* METHODS */
+	
+	public boolean isEmpty() {
+		return count == 0;
+	}
+	
+	public boolean isFull() {
+		return count == size;
+	}
+	
+	public T front() {
+		return queue[first];
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int count() {
+		return count;
+	}
+	
+	public boolean contains(T element) {
+		
+		if (queue == null || isEmpty()) return false;
+		
+		if (last <= first) {
+			
+			for (int i = first; i < size; i++)
+				if (queue[i] == element) return true;
+			
+			for (int j = 0; j < last; j++)
+				if (queue[j] == element) return true;
+				
+		} else {
+			
+			for (int k = first; k < last; k++)
+				if (queue[k] == element) return true;
+			
+		}
+		
+		return false;		
+		
+	}
+	
+	public void enqueue(T new_element) {
+		
+		if (queue == null || isFull()) return;
+		
+		queue[last] = new_element;
+		last = (last + 1) % size;
+		count++;
+		
+	}
+	
+	public void dequeue() {
+		
+		if (queue == null || isEmpty()) return;
+		
+		queue[first] = null;
+		first = (first + 1) % size;
+		count--;
+	}	
+	
+}
